6e15d3de6107731cd92304a99220f7467956f571
정지수 <kia0097@naver.com>
2020-08-03 20:47:57 +0900

    [BFS] 백신

---
+++ goorm/3.1.1.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+/*
+ * 비타알고 2020년 3월 1주차 1번 : 백신(난이도 3)
+ * bfs
+*/
+
+using namespace std;
+int N, M;
+vector<int> v[200001];
+int visit[200001];
+int res[200001];
+
+void bfs(int n){
+
+    if(visit[n] == 1){
+        return;
+    }
+    visit[n] = 1;
+
+    queue<int> q;
+    q.push(n);
+    int count = 0;
+
+    while(!q.empty()){
+        int cur = q.front();
+        q.pop();
+        count++;
+
+        for(int i = 0; i < v[cur].size(); i++){
+            int index = v[cur][i];
+            if(visit[index] == 0){
+                visit[index] = 1;
+                q.push(index);
+            }
+        }
+    }
+
+    res[n] = count;
+}
+
+int main(void){
+
+    cin >> N >> M;
+
+    for(int i = 1; i <= M; i++){
+        int p, q;
+        cin >> p >> q;
+        v[p].push_back(q);
+        v[q].push_back(p);
+    }
+
+    int maxi = 0;
+    int patient = 0;
+
+    for(int i = 1; i <= N; i++){
+        bfs(i);
+
+        if(maxi < res[i]){
+            maxi = res[i];
+            patient = i;
+        }
+    }
+
+    cout << patient << " " << maxi;
+    return 0;
+}
